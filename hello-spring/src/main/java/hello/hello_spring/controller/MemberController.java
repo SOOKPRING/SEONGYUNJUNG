d11576dc36cb3b595c0e8c88788d7ff569a361c8
ynzung <syj91105@sookmyung.ac.kr>
2025-03-11 01:52:40 +0900

    Feat: Controller에 @Controller 선언 및 @Autowired 선언

--- hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
@@ -4,9 +4,11 @@ import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+@Controller
 public class MemberController {
     private final MemberService memberService;
 
+    @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
